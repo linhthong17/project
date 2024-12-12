094510af296c3c05c7265c38c9f5c1d22605e281
linhthong17 <linhthongnguyen1709@gmail.com>
2024-12-13 02:06:31 +0700

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "class.h"
+
+using namespace std;
+
+
+class Userinfo : Item{
+    private:    
+        string userpwd;
+        string userfullname;
+        string userphonenum;
+        string useremail;
+        string useridtype;
+        string userid;
+        string usersellrate;
+        string userbuyrate;
+        string usercp;
+    
+    public:
+        string username;
+
+
+
+};
